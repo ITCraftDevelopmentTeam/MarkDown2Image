fbfbccbe75906ffd9bbf6c9bf904b48f457e2944
This is XiaoDeng <1744793737@qq.com>
2023-06-30 20:15:04 +0800

    支持将image导出为图片

--- markdown2image/__init__.py
+++ markdown2image/__init__.py
@@ -2,8 +2,7 @@ from . import parser
 from . import style
 from rich import print
 
-def md2img(markdown: str, out_path: str):
+def md2img(markdown: str, output_path: str) -> None:
     ast = style.init_style(style.init_links(parser.parse(markdown)))
     size = style.get_size(ast)
-    print(ast)
-    style.draw(ast, size).show()
+    style.draw(ast, size).save(output_path)
